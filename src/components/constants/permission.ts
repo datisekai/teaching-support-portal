4b67d538a3be2f3136b4a69cfd0ae63b15ef2c64
duong91002 <nguyenhaiduong9102@gmail.com>
2024-09-27 04:22:57 +0700

    feat assign permission

--- src/components/constants/permission.ts
+++ src/components/constants/permission.ts
@@ -18,6 +18,259 @@ export const permissions = [
         title: "Xoá",
         value: "delete-user",
       },
+      {
+        title: "Khóa",
+        value: "block-user",
+      },
+      {
+        title: "Reset thiết bị",
+        value: "reset-device-id-user",
+      },
+      {
+        title: "Import",
+        value: "import-user",
+      },
+      {
+        title: "Export",
+        value: "export-user",
+      },
+      {
+        title: "Gán nhóm quyền",
+        value: "assign-group-permission-user",
+      },
+    ],
+  },
+  {
+    title: "Ngành học",
+    children: [
+      {
+        title: "Xem danh sách",
+        value: "view-department",
+      },
+      {
+        title: "Tạo mới",
+        value: "create-department",
+      },
+      {
+        title: "Cập nhật",
+        value: "edit-department",
+      },
+      {
+        title: "Xoá",
+        value: "delete-department",
+      },
+      {
+        title: "Import",
+        value: "import-department",
+      },
+      {
+        title: "Export",
+        value: "export-department",
+      },
+    ],
+  },
+  {
+    title: "Môn học",
+    children: [
+      {
+        title: "Xem danh sách",
+        value: "view-subject",
+      },
+      {
+        title: "Tạo mới",
+        value: "create-subject",
+      },
+      {
+        title: "Cập nhật",
+        value: "edit-subject",
+      },
+      {
+        title: "Xoá",
+        value: "delete-subject",
+      },
+      {
+        title: "Thêm giảng viên",
+        value: "add-teacher-subject",
+      },
+      {
+        title: "Import",
+        value: "import-subject",
+      },
+      {
+        title: "Export",
+        value: "export-subject",
+      },
+    ],
+  },
+  {
+    title: "Lớp học",
+    children: [
+      {
+        title: "Xem danh sách",
+        value: "view-class",
+      },
+      {
+        title: "Tạo mới",
+        value: "create-class",
+      },
+      {
+        title: "Cập nhật",
+        value: "edit-class",
+      },
+      {
+        title: "Xoá",
+        value: "delete-class",
+      },
+      {
+        title: "Thống kê",
+        value: "statistic-class",
+      },
+      {
+        title: "Xem danh sách sinh viên",
+        value: "view-student-class",
+      },
+      {
+        title: "Import",
+        value: "import-class",
+      },
+      {
+        title: "Export",
+        value: "export-class",
+      },
+    ],
+  },
+  {
+    title: "Đơn từ",
+    children: [
+      {
+        title: "Xem danh sách",
+        value: "view-letter",
+      },
+      {
+        title: "Xem",
+        value: "view-detail-letter",
+      },
+    ],
+  },
+  {
+    title: "Thông báo",
+    children: [
+      {
+        title: "Xem danh sách",
+        value: "view-notification",
+      },
+      {
+        title: "Tạo mới",
+        value: "create-notification",
+      },
+      {
+        title: "Cập nhật",
+        value: "edit-notification",
+      },
+      {
+        title: "Xoá",
+        value: "delete-notification",
+      },
+    ],
+  },
+  {
+    title: "Điểm danh",
+    children: [
+      {
+        title: "Xem danh sách",
+        value: "view-attendance",
+      },
+      {
+        title: "Tạo mới",
+        value: "create-attendance",
+      },
+      {
+        title: "Cập nhật",
+        value: "edit-attendance",
+      },
+      {
+        title: "Xoá",
+        value: "delete-attendance",
+      },
+      {
+        title: "Danh sách điểm danh",
+        value: "view-detail-student-attendance",
+      },
+      {
+        title: "Logs",
+        value: "logs-attendance",
+      },
+      {
+        title: "Chi tiết",
+        value: "detail-attendance",
+      },
+    ],
+  },
+  {
+    title: "Sinh viên của lớp",
+    children: [
+      {
+        title: "Xem danh sách",
+        value: "view-user-class",
+      },
+      {
+        title: "Tạo mới",
+        value: "create-user-class",
+      },
+      {
+        title: "Cập nhật",
+        value: "edit-user-class",
+      },
+      {
+        title: "Xoá",
+        value: "delete-user-class",
+      },
+      {
+        title: "Reset thiết bị",
+        value: "reset-device-id-user-class",
+      },
+      {
+        title: "Import",
+        value: "import-user-class",
+      },
+      {
+        title: "Export",
+        value: "export-user-class",
+      },
+    ],
+  },
+  {
+    title: "Phân quyền",
+    children: [
+      {
+        title: "Xem danh sách",
+        value: "view-permission",
+      },
+      {
+        title: "Tạo mới",
+        value: "create-permission",
+      },
+      {
+        title: "Cập nhật",
+        value: "edit-permission",
+      },
+      {
+        title: "Xoá",
+        value: "delete-permission",
+      },
+      {
+        title: "Gán quyền",
+        value: "assign-permission",
+      },
+    ],
+  },
+  {
+    title: "Giao diện",
+    children: [
+      {
+        title: "Cập nhật",
+        value: "edit-interface",
+      },
     ],
   },
 ];
--- src/pages/Permission/PermissionAssign.tsx
+++ src/pages/Permission/PermissionAssign.tsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import MyCard from "../../components/UI/MyCard";
+import { permissions } from "../../components/constants";
+import { InputSwitch, InputSwitchChangeEvent } from "primereact/inputswitch";
+import { Checkbox, CheckboxChangeEvent } from "primereact/checkbox";
 
 const PermissionAssign = () => {
-  return <div>PermissionAssign</div>;
+  const { id } = useParams();
+  const [selectedPermissions, setSelectedPermissions] = useState<string[]>([]);
+  const [checkedStates, setCheckedStates] = useState<{
+    [key: string]: boolean;
+  }>({});
+
+  const togglePermission = (e: CheckboxChangeEvent, item: any) => {
+    const isChecked = e.checked;
+    const updatedPermissions = isChecked
+      ? addPermissions(item)
+      : removePermissions(item);
+
+    setSelectedPermissions(updatedPermissions);
+  };
+
+  const addPermissions = (item: any) => {
+    const newPermissions = [...selectedPermissions, item.title];
+    item.children?.forEach((child: any) => {
+      newPermissions.push(child.value);
+      setCheckedStates((prev) => ({ ...prev, [child.value]: true }));
+    });
+    return newPermissions;
+  };
+
+  const removePermissions = (item: any) => {
+    let newPermissions = selectedPermissions.filter((p) => p !== item.title);
+    item.children?.forEach((child: any) => {
+      newPermissions = newPermissions.filter((p) => p !== child.value);
+      setCheckedStates((prev) => ({ ...prev, [child.value]: false }));
+    });
+    return newPermissions;
+  };
+
+  const handleSwitchChange = (childTitle: string, value: boolean) => {
+    setCheckedStates((prev) => ({ ...prev, [childTitle]: value }));
+  };
+
+  return (
+    <div>
+      <MyCard title="Danh sách quyền">
+        <div className="tw-grid tw-grid-cols-1 md:tw-grid-cols-3 tw-gap-4">
+          {permissions.map((item) => (
+            <div key={item.title} className="tw-p-2 tw-border tw-rounded">
+              <div className="tw-flex tw-items-center">
+                <Checkbox
+                  inputId={item.title}
+                  value={item.title}
+                  onChange={(e) => togglePermission(e, item)}
+                  checked={selectedPermissions.includes(item.title)}
+                />
+                <label
+                  className="tw-ml-2 tw-font-bold tw-text-lg text-primary"
+                  htmlFor={item.title}
+                >
+                  {item.title}
+                </label>
+              </div>
+              {item.children && (
+                <div>
+                  {item.children.map((child) => (
+                    <div
+                      className="tw-mx-4 tw-my-2 tw-flex tw-items-center"
+                      key={child.value}
+                    >
+                      <InputSwitch
+                        checked={checkedStates[child.value] || false}
+                        onChange={(e: InputSwitchChangeEvent) =>
+                          handleSwitchChange(child.value, e.value)
+                        }
+                      />
+                      <label
+                        className="tw-ml-2 tw-font-medium"
+                        htmlFor={child.value}
+                      >
+                        {child.title}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </MyCard>
+    </div>
+  );
 };
 
 export default PermissionAssign;
